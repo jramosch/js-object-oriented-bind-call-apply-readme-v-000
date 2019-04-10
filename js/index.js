0da675701d6058758e33342940081f9253547b37
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-04-10 16:22:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,17 +1,8 @@
-function greet() {
-	console.log(`my name is ${this.name}, hi!`);
-}
-
-greet();
-
-let person = {
-  name: 'Bob',
-  greet: greet
-};
-
-person.greet();
-
 let sally = { name: 'Sally' };
+
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
 
 greet.call(sally);
 
