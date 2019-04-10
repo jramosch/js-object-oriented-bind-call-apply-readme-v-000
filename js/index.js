ab083c4ee112031f8842b50bbdbb6d888d8a76ea
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-04-10 16:25:47 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -13,25 +13,21 @@ greet('Bob');
 greet.bind(sally)('Bob');
 
 class Event {
-    constructor(title, keywords) {
+  constructor(title, keywords) {
         this.title = title;
         this.keywords = keywords;
-    }
+  }
 }
  
 class User {
-    constructor(name, interests) {
+  constructor(name, interests) {
         this.name = name;
         this.interests = interests;
-    }
+  }
  
-    matchInterests(event) {
-        return event.keywords.some(
-            function(word) {
-                return this.interests.includes(word);
-            }.bind(this) // added to the and of the callback function
-        );
-    }
+  matchInterests(event) {
+    return event.keywords.some(word => this.interests.includes(word));
+  }
 }
  
 let billy = new User('billy', ['music', 'art', 'movies']);
